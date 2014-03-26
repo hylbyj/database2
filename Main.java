f28c7634b55efaace4b82f07fb4a9ea349c7c729
hylbyj <hyldestiny@gmail.com>
2014-03-26 17:40:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,18 @@
 public class Main{
           public static void main(String[] args){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    String q = ""; //receive as input as a string q
+                    String escapeQuery = ""; //"URL-escape" the input query q
+                    
+                    //part 1 info box
+                    
+                    
+                    //part 2 question and answer 
+          
+          
+          
+          
+          
+          
+          
+          }
 }
