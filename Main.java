2f912094e5d94d10e6569ddab39aebdf741f92d0
Ariana <ang2144@gmail.com>
2014-03-29 11:00:16 -0400

    added a break when a mid is found with correct entity type

--- Main.java
+++ Main.java
@@ -23,6 +23,7 @@ public class main {
 			query = "Bill Gates";
 			accountKey = "AIzaSyCIQ8gDGEMgxJpSsGK6BwkfLZXtN4MTf4E";
             
+			//load and read content in search API results
 			HttpTransport httpTransport = new NetHttpTransport();
 			HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
 			JSONParser parser = new JSONParser();
@@ -39,14 +40,24 @@ public class main {
             
 			ArrayList<String> midList = new ArrayList<String>();
             
+			//make a list of the mid results
 			for (Object result : results) {
 				midList.add(JsonPath.read(result,"$.mid").toString());
 			}
             
 			System.out.println(midList);
             
+			//define freebase Types
+			String[] freebaseTypeList = {"/people/person", "/book/author", "/film/actor","/tv/tv_actor","/organization/organization_founder",
+                "/business/board_member", "/sports/sports_league", "/sports/sports_team", "/sports/professional_sports_team"};
+			
+			ArrayList<String> entityTypeList = new ArrayList<String>();
+            
+			//running the mids through the Topic API
+        topicloop:
 			for(int i=0; i<midList.size(); i++){
                 
+				//loading Topic API content
 				String topicId = midList.get(i);
 				GenericUrl topicUrl = new GenericUrl("https://www.googleapis.com/freebase/v1/topic" + topicId);
 				topicUrl.put("key", accountKey);
@@ -56,13 +67,29 @@ public class main {
                 
 				ArrayList<String> topicList = new ArrayList<String>();
                 
+				//return first 10 freebase type results
 				for(int j=0; j<10; j++){
-					topicList.add(JsonPath.read(topic,"$.property['/type/object/type'].values["+j+"].text").toString());
+					topicList.add(JsonPath.read(topic,"$.property['/type/object/type'].values["+j+"].id").toString());
 				}
                 
 				System.out.println(topicList);
-                
+				
+				//if the freebase type matches entity type, save in list
+				for(int k=0; k<topicList.size(); k++){
+					for(int l=0; l<freebaseTypeList.length; l++){
+						if(topicList.get(k).equals(freebaseTypeList[l])){
+							entityTypeList.add(topicList.get(k));
+						}
+					}
+					//if we found a match, break out of the loop for running the mids through the topic API
+					if(k == topicList.size()-1 && entityTypeList.size() != 0){
+						break topicloop;
+					}
+				}
 			}
+			
+			System.out.println(entityTypeList);
+			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
